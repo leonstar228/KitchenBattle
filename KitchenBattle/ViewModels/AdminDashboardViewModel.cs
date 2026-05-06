7c602919ef726376884c241b3cf315da3c32e6ac
Vlad <vladyuzevuts@gmail.com>
2026-05-06 20:46:46 +0300

    Add AdminController,AdminService,AdminDashboardViewModel

---
+++ KitchenBattle/ViewModels/AdminDashboardViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace KitchenBattle.ViewModels
+{
+    public class AdminDashboardViewModel
+    {
+        public int TotalUsers { get; set; }
+        public int TotalChefs { get; set; }
+        public int TotalJudges { get; set; }
+
+        public int TotalRecipes { get; set; }
+        public int PublishedRecipes { get; set; }
+        public int PendingRecipes { get; set; }
+        public int RejectedRecipes { get; set; }
+
+        public int TotalBattles { get; set; }
+        public int ActiveBattles { get; set; }
+        public int FinishedBattles { get; set; }
+
+        public double GlobalAverageScore { get; set; }
+        public string MostPopularCategory { get; set; }
+    }
+}
