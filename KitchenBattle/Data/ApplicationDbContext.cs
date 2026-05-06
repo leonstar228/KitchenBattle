862cd658a781bcdea7844e75dae69f4a4480112c
Yevhen <yevhen_danylchenko@ukr.net>
2026-05-06 20:31:46 +0300

    Added navigation properties to models BattleChef and BattleJudge  added ApplicationDbContext. Added HomeController

---
+++ KitchenBattle/Data/ApplicationDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using KitchenBattle.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace KitchenBattle.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly DbContextOptions<ApplicationDbContext> _options;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            _options = options;
+        }
+
+        public DbSet<Recipe> Recipes => Set<Recipe>();
+        public DbSet<Battle> Battles => Set<Battle>();
+        public DbSet<Score> Scores => Set<Score>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Конфігурація для BattleChef
+            modelBuilder.Entity<BattleChef>()
+                .HasKey(bc => new { bc.BattleId, bc.ChefId });
+            modelBuilder.Entity<BattleChef>()
+                .HasOne(bc => bc.Battle)
+                .WithMany(b => b.BattleChefs)
+                .HasForeignKey(bc => bc.BattleId);
+            modelBuilder.Entity<BattleChef>()
+                .HasOne(bc => bc.Chef)
+                .WithMany(c => c.BattleChefs)
+                .HasForeignKey(bc => bc.ChefId);
+            // Конфігурація для BattleJudge
+            modelBuilder.Entity<BattleJudge>()
+                .HasKey(bj => new { bj.BattleId, bj.JudgeId });
+            modelBuilder.Entity<BattleJudge>()
+                .HasOne(bj => bj.Battle)
+                .WithMany(b => b.BattleJudges)
+                .HasForeignKey(bj => bj.BattleId);
+            modelBuilder.Entity<BattleJudge>()
+                .HasOne(bj => bj.Judge)
+                .WithMany(j => j.BattleJudges)
+                .HasForeignKey(bj => bj.JudgeId);
+        }
+    }
+}
