b398dc7cc8709ed4da93c5392a69416f5779e2c4
Yevhen <yevhen_danylchenko@ukr.net>
2026-06-03 20:51:58 +0300

    Edit model edit views edit viewsmodel recipe

--- KitchenBattle/ViewModels/RecipeCreateViewModel.cs
+++ KitchenBattle/ViewModels/RecipeCreateViewModel.cs
@@ -1,11 +1,16 @@
 ﻿using KitchenBattle.Models;
 using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace KitchenBattle.ViewModels
 {
     public class RecipeCreateViewModel
     {
+        
+        [StringLength(50, ErrorMessage = "Назва не може перевищувати 50 символів.")]
         public string Title { get; set; } = string.Empty;
+
+        [StringLength(50, ErrorMessage = "Назва не може перевищувати 50 символів.")]
         public string Description { get; set; } = string.Empty;
         public string Ingredients { get; set; } = string.Empty;
         public int CookingTime { get; set; }
